31fcc972d47ba04c0dc59228ec00db7e06d8750f
wpexia <lihanyang_1993@126.com>
2014-10-30 09:33:40 +0800

    v1.0

--- app/controllers/VacationController.php
+++ app/controllers/VacationController.php
@@ -26,7 +26,7 @@ class VacationController extends BaseController {
 		$bonus = 0;
 		if ($month < 7) {
 			if (isset($surplus[(String)(intval($year) - 1)])) {
-				$bonus = max(0,min($days, (float)($surplus[(String)(intval($year) - 1)][$type])));
+				$bonus = max(0, min($days, (float)($surplus[(String)(intval($year) - 1)][$type])));
 			}
 		}
 		if (!isset($surplus[$year])) {
@@ -61,8 +61,12 @@ class VacationController extends BaseController {
 	public function vacation_stat() {
 		$year = Input::get('year');
 		if (Session::get('userEmail') != 'huanglinjie@baixing.com') {
-			$employee = Employee::getByEmail(Session::get('userEmail'));
-			if(!$employee) return $this->error("请使用百姓网邮箱登陆");
+			$employees = [Employee::getByEmail(Session::get('userEmail'))];
+		} else {
+			$employees = Employee::all();
+		}
+		$tmp = array();
+		foreach ($employees as $employee) {
 			$surplus = json_decode($employee->surplus, true);
 			if (!isset($surplus[$year])) {
 				$surplus[$year] = $employee->initSurplus(intval($year));
@@ -73,11 +77,11 @@ class VacationController extends BaseController {
 			$vacations = Vacation::getByuserAndYear($employee->userId, (int)$year);
 			$tyear = array(
 				'annual' => 0,
-				'sick'   => 0,
+				'sick' => 0,
 			);
 			$lyear = array(
 				'annual' => 0,
-				'sick'   => 0,
+				'sick' => 0,
 			);
 			foreach ($vacations as $vacation) {
 				$days = $vacation['duration'] == 'all' ? 1 : 0.5;
@@ -85,85 +89,84 @@ class VacationController extends BaseController {
 				$lyear[$vacation['type']] += $bonus;
 				$tyear[$vacation['type']] += ($days - $bonus);
 			}
-			$data = array(
-				'result' => 'success',
-				'data' => array(
-					$employee->userId => array(
-						'annual' => array(
-							'normal_used'  => $tyear['annual'],
-							'bonus_used'   => $lyear['annual'],
-							'normal_total' => $tyear['annual']+(float)$surplus[$year]['annual'],
-							'bonus_total'  => $lyear['annual']+(float)$surplus[(String)(intval($year) - 1)]['annual'],
-						),
-						'sick' => array(
-							'normal_used'  => $tyear['sick'],
-							'bonus_used'   => $lyear['sick'],
-							'normal_total' => $tyear['sick']+(float)$surplus[$year]['sick'],
-							'bonus_total'  => $lyear['sick']+(float)$surplus[(String)(intval($year) - 1)]['sick'],
-						),
-						'name'   => $employee->name,
-						'status' => $employee->status,
-					),
+			$tmp[$employee->userId] = array(
+				'annual' => array(
+					'normal_used' => $tyear['annual'],
+					'bonus_used' => $lyear['annual'],
+					'normal_total' => $tyear['annual'] + (float)$surplus[$year]['annual'],
+					'bonus_total' => $lyear['annual'] + (float)$surplus[(String)(intval($year) - 1)]['annual'],
 				),
+				'sick' => array(
+					'normal_used' => $tyear['sick'],
+					'bonus_used' => $lyear['sick'],
+					'normal_total' => $tyear['sick'] + (float)$surplus[$year]['sick'],
+					'bonus_total' => $lyear['sick'] + (float)$surplus[(String)(intval($year) - 1)]['sick'],
+				),
+				'name' => $employee->name,
+				'status' => $employee->status,
 			);
 		}
+		$data = array(
+			'result' => 'success',
+			'data' => $tmp,
+		);
 		return json_encode($data);
 	}
 
-	public function vacation_stat_detail(){
+	public function vacation_stat_detail() {
 		$userId = Input::get('eid');
 		$startDate = Input::get('startDate');
 		$endDate = Input::get('endDate');
 
-		$vacations = Vacation::getByUserAndBeginEnd($userId,$startDate,$endDate);
+		$vacations = Vacation::getByUserAndBeginEnd($userId, $startDate, $endDate);
 		$data = '';
-		foreach($vacations as $vacation){
-			if($data!='')$data .=',';
+		foreach ($vacations as $vacation) {
+			if ($data != '') $data .= ',';
 			$tmp = array(
-				'id'        => $vacation['id'],
-				'date'      => $vacation['date'],
-				'type'      => $vacation['type'],
-				'duration'  => $vacation['duration'],
-				'comment'   => $vacation['comment'],
-				'bonus'     => $vacation['bonus'],
+				'id' => $vacation['id'],
+				'date' => $vacation['date'],
+				'type' => $vacation['type'],
+				'duration' => $vacation['duration'],
+				'comment' => $vacation['comment'],
+				'bonus' => $vacation['bonus'],
 			);
 			$data .= json_encode($tmp);
 		}
 
-		return '{"result":"success","data":['.$data.']}';
+		return '{"result":"success","data":[' . $data . ']}';
 	}
 
-	private function error($msssage){
+	private function error($msssage) {
 		$result = array(
 			'result' => $msssage,
-			'data'   => [],
+			'data' => [],
 		);
 		return json_encode($result);
 	}
 
-	public function xiaojia(){
-		if(Session::get('userEmail')!='huanglinjie@baixing.com'){
+	public function xiaojia() {
+		if (Session::get('userEmail') != 'huanglinjie@baixing.com') {
 			return $this->error('不是管理员暂时还不能销假哦~');
 		}
 		$id = Input::get('id');
-		try{
+		try {
 			$vacation = Vacation::findOrFail(intval($id));
-		}catch (Exception $e){
+		} catch (Exception $e) {
 			return $this->error('销假错误');
 		}
 		$employee = Employee::getByUserId($vacation->userId);
 		$days = $vacation->duration == 'all' ? 1 : 0.5;
 		$bonus = $vacation->bonus;
-		$surplus = json_decode($employee->surplus,true);
-		$year = explode('-',$vacation->date)[0];
-		$surplus[$year][$vacation->type] = (String)((float)$surplus[$year][$vacation->type]+$days-$bonus);
-		$surplus[(String)(intval($year) - 1)][$vacation->type] = (String)((float)$surplus[(String)(intval($year) - 1)][$vacation->type]+$bonus);
+		$surplus = json_decode($employee->surplus, true);
+		$year = explode('-', $vacation->date)[0];
+		$surplus[$year][$vacation->type] = (String)((float)$surplus[$year][$vacation->type] + $days - $bonus);
+		$surplus[(String)(intval($year) - 1)][$vacation->type] = (String)((float)$surplus[(String)(intval($year) - 1)][$vacation->type] + $bonus);
 		$employee->surplus = json_encode($surplus);
 		$employee->save();
 		$vacation->delete();
 		$result = array(
 			'result' => 'success',
-			'data'   => [],
+			'data' => [],
 		);
 		return json_encode($result);
 	}
